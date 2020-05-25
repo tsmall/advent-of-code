5d81bcf43e9c771fb5579196f05d80155fe4492f
Tom Small <tsmall3@gmail.com>
2020-05-25 00:20:49 -0400

    2016/01(clj): add solution to part one

---
+++ 2016/Day01/clj/solution.clj
@@ -0,0 +1,78 @@
+(ns aoc.day01
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is]]))
+
+(def startpos
+  [:north [0 0]])
+
+(defn turn
+  [dir instr]
+  (case [dir instr]
+    [:north :left]  :west
+    [:north :right] :east
+    [:east  :left]  :north
+    [:east  :right] :south
+    [:south :left]  :east
+    [:south :right] :west
+    [:west  :left]  :south
+    [:west  :right] :north))
+
+(deftest test-turning
+  (is (= :west (turn :north :left)))
+  (is (= :south (turn :east :right))))
+
+(defn walk
+  [[x y] dir steps]
+  (case dir
+    :north [x (+ y steps)]
+    :east  [(+ x steps) y]
+    :south [x (- y steps)]
+    :west  [(- x steps) y]))
+
+(defn move
+  [[dir pos] instr steps]
+  (let [new-dir (turn dir instr)
+        new-pos (walk pos new-dir steps)]
+    [new-dir new-pos]))
+
+(deftest test-move
+  (is (= [:east [0 5]] (move startpos :right 5))))
+
+(defn blocks-away
+  [pos]
+  (apply + (map #(Math/abs %1) pos)))
+
+(defn parse
+  [s]
+  (let [dir (case (.substring s 0 1)
+              "L" :left
+              "R" :right)
+        steps (Integer/parseInt (.substring s 1))]
+    [dir steps]))
+
+(deftest test-parse
+  (is (= [:right 2] (parse "R2")))
+  (is (= [:right 5] (parse "R5")))
+  (is (= [:left 3] (parse "L3")))
+  (is (= [:left 12] (parse "L12"))))
+
+(defn parse-input
+  [s]
+  (->> (str/split s #", ")
+       (map parse)))
+
+(defn part1
+  [input]
+  (let [end (reduce #(apply move %1 %2)
+                    startpos
+                    (parse-input input))
+        [dir pos] end]
+    (blocks-away pos)))
+
+(deftest test-part1-examples
+  (is (= 5 (part1 "R2, L3")))
+  (is (= 2 (part1 "R2, R2, R2")))
+  (is (= 12 (part1 "R5, L5, R5, R3"))))
+
+(comment
+  (part1 (str/trim (slurp "../input.txt"))))
