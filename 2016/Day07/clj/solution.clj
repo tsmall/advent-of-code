0e9d90d07dbb7206cfd9b42b11fa4e936bb7db58
Tom Small <tsmall3@gmail.com>
2020-09-04 22:27:24 -0400

    2016/07(clj): add solution to part two

--- 2016/Day07/clj/solution.clj
+++ 2016/Day07/clj/solution.clj
@@ -15,11 +15,49 @@
   (some abba? (partition 4 1 s)))
 
 
+(defn aba?
+  [xs]
+  (let [[a b c] xs]
+    (and (= a c)
+         (not= a b))))
+
+
+(defn bab?
+  [aba xs]
+  (let [[a b c] aba
+        [x y z] xs]
+    (and (aba? xs)
+         (= a y)
+         (= b x))))
+
+
+(deftest test-bab?
+  (is (bab? "aba" "bab"))
+  (is (bab? "xyx" "yxy"))
+  (is (not (bab? "xyx" "xyx")))
+  (is (not (bab? "aaa" "aaa"))))
+
+
+(defn abas
+  [s]
+  (filter aba? (partition 3 1 s)))
+
+
+(defn contains-bab?
+  [aba s]
+  (some (partial bab? aba) (partition 3 1 s)))
+
+
+(deftest test-contains-bab?
+  (is (contains-bab? "aba" "cdbabef"))
+  (is (contains-bab? "bzb" "zazbz")))
+
+
 (defn categorize
   [match]
   (if (nil? (first match))
-    :hyper
-    :safe))
+    :hypernet
+    :supernet))
 
 
 (defn parse
@@ -28,22 +66,22 @@
         m (->> (re-seq re s)
                (map (partial drop 1))
                (group-by categorize))]
-    {:safe (mapcat (partial take 1) (:safe m))
-     :hyper (mapcat (partial drop 1) (:hyper m))}))
+    {:supernet (mapcat (partial take 1) (:supernet m))
+     :hypernet (mapcat (partial drop 1) (:hypernet m))}))
 
 
 (deftest test-parse
-  (is (= {:safe ["abba" "qrst"], :hyper ["mnop"]}
+  (is (= {:supernet ["abba" "qrst"], :hypernet ["mnop"]}
          (parse "abba[mnop]qrst")))
-  (is (= {:safe ["abba" "fghi" "nopq"], :hyper ["cdde" "jklm"]}
+  (is (= {:supernet ["abba" "fghi" "nopq"], :hypernet ["cdde" "jklm"]}
          (parse "abba[cdde]fghi[jklm]nopq"))))
 
 
 (defn supports-tls?
   [s]
-  (let [{:keys [safe hyper]} (parse s)]
-    (and (not (some contains-abba? hyper))
-         (some contains-abba? safe))))
+  (let [{:keys [supernet hypernet]} (parse s)]
+    (and (not (some contains-abba? hypernet))
+         (some contains-abba? supernet))))
 
 
 (deftest test-supports-tls?
@@ -53,6 +91,22 @@
   (is (supports-tls? "ioxxoj[asdfgh]zxcvbn"))
   (is (supports-tls? "abcd[jklm]oxxo"))
   (is (supports-tls? "abba[cdde]fghi[jklm]nopq")))
+
+
+(defn supports-ssl?
+  [s]
+  (let [{:keys [supernet hypernet]} (parse s)
+        abas (mapcat abas supernet)]
+    (some identity
+     (for [aba abas, s hypernet]
+       (contains-bab? aba s)))))
+
+
+(deftest test-supports-ssl?
+  (is (supports-ssl? "aba[bab]xyz"))
+  (is (not (supports-ssl? "xyx[xyx]xyx")))
+  (is (supports-ssl? "aaa[kek]eke"))
+  (is (supports-ssl? "zazbz[bzb]cdb")))
 
 
 (defn part1
@@ -73,7 +127,27 @@
     (is (= 2 (part1 s)))))
 
 
+(defn part2
+  ([]
+   (part2 (slurp "../input.txt")))
+  ([s]
+   (->> (str/split-lines s)
+        (filter supports-ssl?)
+        (count))))
+
+
+(deftest test-part2
+  (let [lines ["aba[bab]xyz"
+               "xyx[xyx]xyx"
+               "aaa[kek]eke"
+               "zazbz[bzb]cdb"
+               "aba[xyz]ded[ede]fgh"]
+        s (str/join "\n" lines)]
+    (is (= 4 (part2 s)))))
+
+
 (comment
   (run-all-tests)
   (part1)
+  (part2)
   )
