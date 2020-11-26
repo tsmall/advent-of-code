e22f57d5fd9297aeac729e46b7e6ed2bc7a4d7e1
Tom Small <tsmall3@gmail.com>
2020-11-25 23:56:07 -0500

    2020/prep(zig): add per-line and -number parsing

--- 2020/prep/example.zig
+++ 2020/prep/example.zig
@@ -1,55 +1,79 @@
 const std = @import("std");
+const fmt = std.fmt;
 const fs  = std.fs;
 const io  = std.io;
 
 pub fn main() !void {
-    const input_file = try fs.cwd().openFile("chars.txt", .{});
-    defer input_file.close();
+    const stdout = io.getStdOut().writer();
 
-    var elevator = Elevator{};
-    var monitor = Monitor{};
+    {
+        const char_file = try InputFile.new("chars.txt");
+        defer char_file.close();
 
-    const input = input_file.reader();
-    while (true) {
-        const char = input.readByte() catch |err| switch (err) {
-            error.EndOfStream => break,
+        while (try char_file.nextChar()) |char| {
+            try stdout.print("CHAR: {c}\n", .{char});
+        }
+    }
+
+    {
+        const line_file = try InputFile.new("lines.txt");
+        defer line_file.close();
+
+        var buffer: [100]u8 = undefined;
+        while (try line_file.nextLine(buffer[0..])) |line| {
+            try stdout.print("LINE: {s}\n", .{line});
+        }
+    }
+
+    try stdout.print("\n", .{});
+
+    {
+        const number_file = try InputFile.new("numbers.txt");
+        defer number_file.close();
+
+        var buffer: [100]u8 = undefined;
+        while (try number_file.nextInt(i64, buffer[0..])) |n| {
+            try stdout.print("NUMBER: {d}\n", .{n});
+        }
+    }
+}
+
+
+const InputFile = struct {
+    file: fs.File,
+    reader: fs.File.Reader,
+
+    fn new(path: []const u8) !InputFile {
+        const file = try fs.cwd().openFile(path, .{});
+        return InputFile {
+            .file = file,
+            .reader = file.reader(),
+        };
+    }
+
+    fn close(self: *const InputFile) void {
+        self.file.close();
+    }
+
+    fn nextChar(self: *const InputFile) !?u8 {
+        const char = self.reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => return null,
             else => |e| return e,
         };
 
-        elevator.move(char);
-        monitor.observe(elevator);
+        return char;
     }
 
-    const stdout = io.getStdOut().writer();
-    try stdout.print("Part 1: {d}\n", .{elevator.floor});
-    try stdout.print("Part 2: {d}\n", .{monitor.basement});
-}
-
-const Elevator = struct {
-    floor: i64 = 0,
-
-    pub fn move(self: *Elevator, char: u8) void {
-        switch (char) {
-            '(' => self.floor += 1,
-            ')' => self.floor -= 1,
-            else => {},
-        }
+    fn nextLine(self: *const InputFile, buffer: []u8) !?[]u8 {
+        return try self.reader.readUntilDelimiterOrEof(buffer, '\n');
     }
-};
 
-const Monitor = struct {
-    index: u64 = 1,
-    basement: u64 = 0,
-
-    pub fn observe(self: *Monitor, elevator: Elevator) void {
-        if (self.basement != 0) {
-            return;
+    fn nextInt(self: *const InputFile, comptime T: type, buffer: []u8) !?T {
+        const string = try self.reader.readUntilDelimiterOrEof(buffer, ' ');
+        if (string) |s| {
+            return try fmt.parseInt(T, s, 10);
+        } else {
+            return null;
         }
-        
-        if (elevator.floor < 0) {
-            self.basement = self.index;
-        }
-
-        self.index += 1;
     }
 };
