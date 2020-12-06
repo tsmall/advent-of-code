fd4b93bdb7c823fe3340fb382d225480359a0970
Tom Small <tsmall3@gmail.com>
2020-12-06 14:00:51 -0500

    2020/06(c): add solution to part two

--- 2020/06/c/solution.c
+++ 2020/06/c/solution.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,28 +9,41 @@ question_index (char letter)
 }
 
 void
-reset (bool questions[26])
+reset (int questions[26])
 {
   for (int i = 0; i < 26; i++)
-    questions[i] = false;
+    questions[i] = 0;
 }
 
 void
-record_answers (bool questions[26], char *answers, int answer_length)
+record_answers (int questions[26], char *answers, int answer_length)
 {
   for (int i = 0; i < answer_length; i++)
     {
       int index = question_index (answers[i]);
-      questions[index] = true;
+      questions[index]++;
     }
 }
 
 int
-yes_count (bool questions[26])
+yes_count (const int questions[26])
 {
   int count = 0;
   for (int i = 0; i < 26; i++)
-    if (questions[i])
+    {
+      if (questions[i] > 0)
+        count++;
+    }
+
+  return count;
+}
+
+int
+agreed_count (const int questions[26], int party_size)
+{
+  int count = 0;
+  for (int i = 0; i < 26; i++)
+    if (questions[i] == party_size)
       count++;
 
   return count;
@@ -41,8 +53,10 @@ int
 main (int argc, char **argv)
 {
   int total_yes_answers = 0;
+  int total_agreed_answers = 0;
 
-  bool questions[26];
+  int party_size = 0;
+  int questions[26];
   reset (questions);
 
   size_t line_length = 50;
@@ -50,16 +64,27 @@ main (int argc, char **argv)
   int chars_read;
   while ((chars_read = getline (&line, &line_length, stdin)) > 0)
     {
-      record_answers (questions, line, line_length);
       if (line[0] == '\n')
         {
+
           total_yes_answers += yes_count (questions);
+          total_agreed_answers += agreed_count (questions, party_size);
+
           reset (questions);
+          party_size = 0;
+
+          continue;
         }
+
+      party_size++;
+      record_answers (questions, line, chars_read - 1);
     }
+
   total_yes_answers += yes_count (questions);
+  total_agreed_answers += agreed_count (questions, party_size);
 
   printf ("Part 1: %d\n", total_yes_answers);
+  printf ("Part 2: %d\n", total_agreed_answers);
 
   free (line);
   return 0;
