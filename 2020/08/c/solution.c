13a4c945303f74d746a16a1961b69f2f5e8a96f7
Tom Small <tsmall3@gmail.com>
2020-12-12 17:05:45 -0500

    2020/08(c): add solution to part two

--- 2020/08/c/solution.c
+++ 2020/08/c/solution.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,15 +24,18 @@ struct instruction_t
 };
 
 
-int accumulator = 0;
-int program_counter = 0;
+int accumulator;
+int program_counter;
 instruction_t memory[MEMORY_SIZE];
 int execution_count[MEMORY_SIZE];
 
 
 void
-initialize_cpu ()
+reset_cpu ()
 {
+  accumulator = 0;
+  program_counter = 0;
+
   for (int i = 0; i < MEMORY_SIZE; i++)
     execution_count[i] = 0;
 }
@@ -93,16 +97,99 @@ run_instruction ()
 }
 
 
+typedef enum termination_condition_t termination_condition_t;
+enum termination_condition_t
+{
+  INFINITE_LOOP,
+  NORMAL,
+};
+
+
+termination_condition_t
+run_to_end_or_loop ()
+{
+  while (1)
+    {
+      if ((++execution_count[program_counter]) == 2)
+        return INFINITE_LOOP;
+
+      if (program_counter >= MEMORY_SIZE)
+        return NORMAL;
+
+      run_instruction ();
+    }
+}
+
+
+bool
+is_swappable_instruction (int index)
+{
+  instruction_t *instruction = &memory[index];
+  return (instruction->operation == NOP
+          || instruction->operation == JMP);
+}
+
+
+void
+swap_instruction (int index)
+{
+  instruction_t *instruction = &memory[index];
+  switch (instruction->operation)
+    {
+      case NOP:
+        instruction->operation = JMP;
+        break;
+
+      case JMP:
+        instruction->operation = NOP;
+        break;
+
+      case ACC:
+        // Shouldn't be changed
+        break;
+    }
+}
+
+
+int
+change_next_instruction (int start_index)
+{
+  if (start_index >= 0)
+    swap_instruction (start_index);
+
+  for (int i = start_index + 1; i < MEMORY_SIZE; i++)
+    {
+      if (is_swappable_instruction (i))
+        {
+          swap_instruction (i);
+          return i;
+        }
+    }
+
+  fprintf (stderr, "ERROR No instructions to change\n");
+  exit (1);
+}
+
+
 int
 main (int argc, char **argv)
 {
-  initialize_cpu ();
+  reset_cpu ();
   load_memory ();
 
-  while ((++execution_count[program_counter]) < 2)
-    run_instruction ();
-
+  run_to_end_or_loop ();
   printf ("Part 1: %d\n", accumulator);
+
+  int last_instruction_changed = -1;
+  termination_condition_t result = INFINITE_LOOP;
+  while (result != NORMAL)
+    {
+      reset_cpu ();
+      last_instruction_changed = change_next_instruction (last_instruction_changed);
+      result = run_to_end_or_loop ();
+    }
+
+  printf ("Part 2: %d\n", accumulator);
 
   return 0;
 }
