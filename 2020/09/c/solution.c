39a3297b87730620fc8aeebe8a0ced3bcf2f8963
Tom Small <tsmall3@gmail.com>
2020-12-12 17:44:33 -0500

    2020/09(c): add solution to part two

--- 2020/09/c/solution.c
+++ 2020/09/c/solution.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -31,6 +32,11 @@ main (int argc, char **argv)
   int buffer[PREAMBLE_LENGTH];
   load_preamble (buffer);
 
+  int all_numbers[1000];
+  int all_numbers_index = 0;
+  for (int i = 0; i < PREAMBLE_LENGTH; i++)
+    all_numbers[all_numbers_index++] = buffer[i];
+
   int oldest_index = 0;
   int next_number;
   while ((scanf ("%d", &next_number)) == 1)
@@ -40,9 +46,39 @@ main (int argc, char **argv)
 
       buffer[oldest_index] = next_number;
       oldest_index = (oldest_index + 1) % PREAMBLE_LENGTH;
+
+      all_numbers[all_numbers_index++] = next_number;
+      assert (all_numbers_index < 1000);
     }
 
   printf ("Part 1: %d\n", next_number);
+
+  int sum;
+  int smallest;
+  int largest;
+  for (int i = 0; i < all_numbers_index; i++)
+    {
+      sum = 0;
+      smallest = __INT_MAX__;
+      largest = 0;
+
+      int n;
+      for (int j = i; j < all_numbers_index; j++)
+        {
+          n = all_numbers[j];
+          sum += n;
+          if (n < smallest)
+            smallest = n;
+          if (n > largest)
+            largest = n;
+
+          if (sum == next_number)
+            goto DONE;
+        }
+    }
+
+  DONE:
+  printf ("Part 2: %d\n", smallest + largest);
 
   return 0;
 }
