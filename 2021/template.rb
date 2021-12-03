635ebec763545fd5b576d83003fc159c9738b0d6
Tom Small <tsmall3@gmail.com>
2021-12-02 21:41:39 -0500

    2021(ruby): add a template file

---
+++ 2021/template.rb
@@ -0,0 +1,27 @@
+# Advent of Code 2021
+# Day X: TODO
+
+module Day0X
+  module_function
+
+  def run
+    puts "Part 1: #{part_one}"
+    puts "Part 2: #{part_two}"
+  end
+
+  def part_one
+    0
+  end
+
+  def part_two
+    0
+  end
+
+  def input
+    File.open("../input.txt")
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  Day0X.run
+end
