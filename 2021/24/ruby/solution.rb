efb3193e9bf38b2a390ee38d7f361437d84f49e9
Tom Small <tsmall3@gmail.com>
2021-12-24 14:37:31 -0500

    2021/24(ruby): add optimized solution for parts one and two

--- 2021/24/ruby/solution.rb
+++ 2021/24/ruby/solution.rb
@@ -9,133 +9,56 @@ module Day24
     puts "Part 2: #{part_two}"
   end
 
-  def part_one(instructions=nil)
-    instructions ||= input
-    program = Program.new(instructions)
-
-    each_model_number do |n|
-      print "\rTrying a new serial number. [n=#{n}]"
-      alu = ALU.new(n)
-      program.run(alu)
-      return n if alu.z == 0
-    end
-
-    nil
-  end
-
-  def each_model_number
-    n = 99_999_999_999_999
-    while n > 9_999_999_999_999
-      yield n
-
-      n -= 1
-      while n.to_s.include? '0'
-        n -= 1
-      end
-    end
+  def part_one(text=nil)
+    text ||= input
+    a, b = parse(text)
+    calculate(a, b, minimize: false)
   end
 
   def part_two(text=nil)
     text ||= input
-    0
+    a, b = parse(text)
+    calculate(a, b, minimize: true)
+  end
+
+  def calculate(a, b, minimize:)
+    digits = [0] * 14
+    stack = []
+
+    (0..13).each do |i|
+      if a[i] > 0
+        stack << [i, b[i]]
+      else
+        ix, v = stack.pop()
+        v += a[i]
+
+        if minimize
+          digits[ix] = 1 - [0, v].min
+          digits[i]  = 1 + [0, v].max
+        else
+          digits[ix] = 9 - [0, v].max
+          digits[i]  = 9 + [0, v].min
+        end
+      end
+    end
+
+    digits.inject(0) { |total, n| total * 10 + n }
+  end
+
+  def parse(text)
+    a = []
+    b = []
+
+    text.each_line.each_with_index do |line, i|
+      a << line.split.last.to_i if i % 18 ==  5
+      b << line.split.last.to_i if i % 18 == 15
+    end
+
+    [a, b]
   end
 
   def input
     File.open("../input.txt")
-  end
-end
-
-class Program
-  attr_accessor :instructions
-
-  def initialize(instructions)
-    @instructions = instructions.each_line
-  end
-
-  def run(alu)
-    instructions.rewind
-
-    instructions.each do |line|
-      command, *args = parse(line)
-      alu.send(command, *args)
-      # puts "Ran instruction. [command=#{command} args=#{args.inspect}]"
-    end
-  end
-
-  def parse(line)
-    command, *args = line.strip.split
-
-    n = args.last.to_i
-    args[-1] = n if n != 0
-
-    [command, *args]
-  end
-end
-
-class ALU
-  attr_accessor :w, :x, :y, :z
-  attr_accessor :inputs
-
-  def initialize(input_number)
-    @w = @x = @y = @z = 0
-    @inputs = input_number.to_s.each_char.lazy.collect(&:to_i)
-  end
-
-  def inp(register)
-    write(register, inputs.next)
-  end
-
-  def add(register_a, register_b)
-    a = read(register_a)
-    b = read(register_b)
-    write(register_a, a + b)
-  end
-
-  def mul(register_a, register_or_value_b)
-    a = read(register_a)
-    b = read(register_or_value_b)
-    write(register_a, a * b)
-  end
-
-  def div(register_a, register_b)
-    a = read(register_a)
-    b = read(register_b)
-    write(register_a, a / b)
-  end
-
-  def mod(register_a, register_b)
-    a = read(register_a)
-    b = read(register_b)
-    write(register_a, a % b)
-  end
-
-  def eql(register_a, register_b)
-    a = read(register_a)
-    b = read(register_b)
-    write(register_a, a == b ? 1 : 0)
-  end
-
-  def read(register_or_value)
-    if register?(register_or_value)
-      send("#{register_or_value}")
-    else
-      register_or_value.to_i
-    end
-  end
-
-  def write(register, n)
-    check(register)
-    send("#{register}=", n)
-  end
-
-  def check(register)
-    if not register?(register)
-      raise ArgumentError, "Invalid register (#{register})"
-    end
-  end
-
-  def register?(register)
-    instance_variables.include? "@#{register}".to_sym
   end
 end
 
